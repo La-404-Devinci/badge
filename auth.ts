c59923d005064d94f6d122800e2cb82e39377ed7
Paul Boisaubert <146094035+LeCodeurDeParis@users.noreply.github.com>
2025-05-07 11:44:38 +0200

    feature: finished connexion with google discord and github

--- auth.ts
+++ auth.ts
@@ -29,5 +29,9 @@ export const auth = betterAuth({
             clientId: process.env.DISCORD_CLIENT_ID as string,
             clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
         },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
     },
 });
