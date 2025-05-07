6a774730ca78e473c8d6eda0b5e6b63cc8650b6d
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-05-07 20:51:51 +0200

    refactor: reorganize imports in onboarding page component

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import PersonalStep from "./_components/personal-step";
-import RoleStep from "./_components/role-step";
-import PositionStep from "./_components/position-step";
 import BackButton from "./_components/back-button";
+import PersonalStep from "./_components/personal-step";
+import PositionStep from "./_components/position-step";
+import RoleStep from "./_components/role-step";
 import { useStepStore } from "./_components/store";
 
 export default function Onboarding() {
