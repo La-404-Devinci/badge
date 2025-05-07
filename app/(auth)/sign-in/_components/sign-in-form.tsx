f3bf169a35e33fa993e6c13d7ec6acd79e6f1036
Nicolas Becharat <nicolas.becharat@deskare.io>
2025-05-07 09:17:52 +0200

    refactor: remove unused router import

--- app/(auth)/sign-in/_components/sign-in-form.tsx
+++ app/(auth)/sign-in/_components/sign-in-form.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 
-import { useRouter } from "next/navigation";
 import { useTranslations } from "next-intl";
 
 import { StaggeredFadeLoader } from "@/components/staggered-fade-loader";
