1d99ffc8d98f4e9ed0948fe2755220012f6bcf84
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-05-08 17:34:30 +0200

    feat: automatically proceed to next step for specific user roles in onboarding

--- app/(auth)/onboarding/_components/role-step.tsx
+++ app/(auth)/onboarding/_components/role-step.tsx
@@ -1,10 +1,28 @@
 import { RiUserLine } from "@remixicon/react";
 import { useTranslations } from "next-intl";
 
+import { USER_ROLES } from "@/constants/roles";
+import { useUserData } from "@/hooks/use-user-data";
+
 import HeaderStep from "./header-step";
 import { RoleSelection } from "./role-selection";
+import { useStepStore } from "./store";
+
 export default function RoleStep() {
     const t = useTranslations("auth");
+
+    const { user } = useUserData();
+
+    const { nextStep } = useStepStore();
+
+    if (
+        user?.role === USER_ROLES.ADMIN ||
+        user?.role === USER_ROLES.MEMBER ||
+        user?.role === USER_ROLES.EXTERNAL
+    ) {
+        nextStep();
+    }
+
     return (
         <div className="flex flex-col gap-6">
             <HeaderStep
