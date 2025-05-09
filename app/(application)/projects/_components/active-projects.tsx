b8cb29324985e4aa9ac1335f91717928ad8b5f34
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-05-10 01:24:19 +0200

    feat: create ActiveProjects component to display user's active projects with loading state

---
+++ app/(application)/projects/_components/active-projects.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { CursorLoader } from "@/components/ui/cursor-loader";
+import { useUserData } from "@/hooks/use-user-data";
+import { useTRPC } from "@/trpc/client";
+
+import { ProjectCard } from "./card";
+
+export default function ActiveProjects() {
+    const { user } = useUserData();
+
+    const trpc = useTRPC();
+
+    const { data, isLoading } = useQuery({
+        ...trpc.project.getProjects.queryOptions(),
+    });
+
+    return (
+        <div className="flex flex-col gap-6 px-4 pb-6 lg:px-8 lg:pt-1">
+            <div className="flex flex-col gap-6">
+                <div className="flex items-center justify-between">
+                    <h2 className="text-label-md font-semibold">
+                        Active Projects
+                    </h2>
+                </div>
+            </div>
+
+            {isLoading ? (
+                <div className="flex items-center justify-center">
+                    <CursorLoader className="size-4 animate-spin" />
+                </div>
+            ) : (
+                <div className="grid grid-cols-[repeat(auto-fill,minmax(344px,1fr))] items-start justify-center gap-6">
+                    {data?.data.map((project) => (
+                        <ProjectCard
+                            key={project.id}
+                            {...project}
+                            user={user!}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
--- app/(application)/projects/_components/card.tsx
+++ app/(application)/projects/_components/card.tsx
@@ -6,23 +6,26 @@ import {
     RiClockwise2Fill,
     RiGithubLine,
     RiMessage2Line,
-    RiUserLine,
-    RiCheckFill,
     RiDiscordFill,
+    RiFileTextLine,
+    RiArrowUpFill,
+    RiArrowDownFill,
 } from "@remixicon/react";
 import { motion, AnimatePresence } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
 
-import * as Avatar from "@/components/ui/avatar";
+/* import * as     Avatar from "@/components/ui/avatar";*/
 import * as Badge from "@/components/ui/badge";
 import * as Button from "@/components/ui/button";
 import * as ProgressBar from "@/components/ui/progress-bar";
 import * as Tooltip from "@/components/ui/tooltip";
+import { USER_ROLES } from "@/constants/roles";
+import { User } from "@/db/schema";
 interface ProjectStatusCardProps {
     title: string;
-    contributors: Array<{ name: string; image?: string }>;
-    skills: Array<{ title: string; completed: boolean }> | null;
+    /*     contributors: Array<{ name: string; image?: string }> | null;
+    skills: Array<{ title: string; completed: boolean }> | null; */
     id: string;
     description: string;
     badgeName: string;
@@ -31,19 +34,22 @@ interface ProjectStatusCardProps {
     endDate: Date;
     type: string;
     exclusive404: boolean;
+    status: string;
+    user: User;
 }
 
 export function ProjectCard({
     title,
     endDate,
-    contributors,
-    skills,
+    /*     contributors,
+    skills, */
     badgeName,
     badgeImage,
     startDate,
     type,
     exclusive404,
     id,
+    user,
 }: ProjectStatusCardProps) {
     const [isExpanded, setIsExpanded] = React.useState(false);
     const toggleExpand = () => setIsExpanded((prev) => !prev);
@@ -59,10 +65,7 @@ export function ProjectCard({
 
     return (
         <div>
-            <div
-                className="w-full max-w-md cursor-pointer transition-all duration-300 hover:shadow-lg rounded-lg border border-border-soft-200 p-4"
-                onClick={toggleExpand}
-            >
+            <div className="w-full max-w-md cursor-pointer transition-all duration-300 hover:shadow-lg rounded-lg border border-neutral-200 p-4">
                 <div className="space-y-1">
                     <div className="flex justify-between items-start w-full">
                         <div className="space-y-2">
@@ -114,11 +117,19 @@ export function ProjectCard({
                             <Tooltip.Root>
                                 <Tooltip.Trigger asChild>
                                     <Button.Root variant="neutral" size="small">
-                                        <RiGithubLine className="size-4" />
+                                        {type === "dev" ? (
+                                            <RiGithubLine className="size-4" />
+                                        ) : (
+                                            <RiFileTextLine className="size-4" />
+                                        )}
                                     </Button.Root>
                                 </Tooltip.Trigger>
                                 <Tooltip.Content>
-                                    <p>View on GitHub</p>
+                                    {type === "dev" ? (
+                                        <p>View on GitHub</p>
+                                    ) : (
+                                        <p>View on Figma</p>
+                                    )}
                                 </Tooltip.Content>
                             </Tooltip.Root>
                         </div>
@@ -132,6 +143,18 @@ export function ProjectCard({
                             <span>{progress}%</span>
                         </div>
                         <ProgressBar.Root value={progress} className="h-2" />
+                        <Button.Root
+                            variant="neutral"
+                            mode="stroke"
+                            className="w-full"
+                            onClick={toggleExpand}
+                        >
+                            {isExpanded ? (
+                                <RiArrowUpFill className="size-4" />
+                            ) : (
+                                <RiArrowDownFill className="size-4" />
+                            )}
+                        </Button.Root>
                     </div>
 
                     <motion.div
@@ -165,48 +188,53 @@ export function ProjectCard({
                                                 </span>
                                             </div>
                                         </div>
-
+                                        {/*
                                         <div className="space-y-2">
+                                           
                                             <h4 className="font-medium text-sm flex items-center">
                                                 <RiUserLine className="h-4 w-4 mr-2" />
                                                 Contributors
                                             </h4>
                                             <div className="flex -space-x-2">
-                                                {contributors.map(
-                                                    (contributor, index) => (
-                                                        <Tooltip.Root
-                                                            key={index}
-                                                        >
-                                                            <Tooltip.Trigger
-                                                                asChild
+                                                {contributors &&
+                                                    contributors.map(
+                                                        (
+                                                            contributor,
+                                                            index
+                                                        ) => (
+                                                            <Tooltip.Root
+                                                                key={index}
                                                             >
-                                                                <Avatar.Root className="border-2 border-white size-8">
-                                                                    <Avatar.Image
-                                                                        src={
-                                                                            contributor.image ||
-                                                                            `/placeholder.svg?height=32&width=32&text=${contributor.name[0]}`
-                                                                        }
-                                                                        alt={
+                                                                <Tooltip.Trigger
+                                                                    asChild
+                                                                >
+                                                                    <Avatar.Root className="border-2 border-white size-8">
+                                                                        <Avatar.Image
+                                                                            src={
+                                                                                contributor.image ||
+                                                                                `/placeholder.svg?height=32&width=32&text=${contributor.name[0]}`
+                                                                            }
+                                                                            alt={
+                                                                                contributor.name
+                                                                            }
+                                                                            className="size-8"
+                                                                        />
+                                                                    </Avatar.Root>
+                                                                </Tooltip.Trigger>
+                                                                <Tooltip.Content>
+                                                                    <p className="text-sm">
+                                                                        {
                                                                             contributor.name
                                                                         }
-                                                                        className="size-8"
-                                                                    />
-                                                                </Avatar.Root>
-                                                            </Tooltip.Trigger>
-                                                            <Tooltip.Content>
-                                                                <p className="text-sm">
-                                                                    {
-                                                                        contributor.name
-                                                                    }
-                                                                </p>
-                                                            </Tooltip.Content>
-                                                        </Tooltip.Root>
-                                                    )
-                                                )}
+                                                                    </p>
+                                                                </Tooltip.Content>
+                                                            </Tooltip.Root>
+                                                        )
+                                                    )}
                                             </div>
                                         </div>
 
-                                        <div className="space-y-2">
+                                         <div className="space-y-2">
                                             <h4 className="font-medium text-sm">
                                                 Skills required
                                             </h4>
@@ -224,9 +252,22 @@ export function ProjectCard({
                                                         )}
                                                     </div>
                                                 ))}
-                                        </div>
+                                        </div> */}
 
-                                        <div className="space-y-2">
+                                        <div className="space-y-2 flex flex-col gap-2">
+                                            <Button.Root
+                                                className={`w-full`}
+                                                variant="neutral"
+                                                mode="stroke"
+                                                disabled={
+                                                    exclusive404
+                                                        ? user.role !==
+                                                          USER_ROLES.MEMBER
+                                                        : false
+                                                }
+                                            >
+                                                Want to participate?
+                                            </Button.Root>
                                             <Button.Root
                                                 className="w-full"
                                                 asChild
